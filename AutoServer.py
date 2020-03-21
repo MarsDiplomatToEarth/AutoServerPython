753c93b967981fa75a118b43dad69becc19b1ae6
MarsDiplomatToEarth <60285644+MarsDiplomatToEarth@users.noreply.github.com>
2020-03-21 20:24:10 +0800

    Update AutoServer.py

--- AutoServer.py
+++ AutoServer.py
@@ -25,40 +25,58 @@ class AutoServer:
             print("device " + self.address.replace("http://","").replace("/","") + " is off\nplease download the apk from github, run it on your rooted device and open it")
             exit()
 
+    #function for use
+    def requestdata(self,url):
+        return json.loads(requests.get(url).text)["data"]
+
+
     #funtion for control device
+    def tsleep(self,t):
+        time.sleep(t)
+
     def tap(self,x,y):
-        result = requests.get(self.address+"shellr?cmd="+"input tap "+ str(x)+" "+str(y) ).text
+        result = requests.get(self.address+"shell?cmd="+"input tap "+ str(x)+" "+str(y) ).text
+        print(result)
+        return result
+
+    def tapbound(self,b):
+        result = requests.get(self.address+"shell?cmd="+"input tap "+ str(b[0])+" "+str(b[1]) ).text
         print(result)
         return result
 
     def swipe(self,x1,y1,x2,y2):
-        result = requests.get(self.address+"shellr?cmd="+"input swipe "+ str(x1)+" "+str(y1)+ " "+str(x2)+" "+str(y2) ).text
+        result = requests.get(self.address+"shell?cmd="+"input swipe "+ str(x1)+" "+str(y1)+ " "+str(x2)+" "+str(y2) ).text
         print(result)
         return result
 
     def input(self,text):
-        result = requests.get(self.address+"shellr?cmd="+"input text "+str(text) ).text
+        result = requests.get(self.address+"shell?cmd="+"input text '"+str(text)+"'" ).text
+        print(result)
+        return result
+
+    def inputu(self,text):
+        result = requests.get(self.address+"input?str='"+str(text)+"'" ).text
         print(result)
         return result
 
     #basic control
     def wakeup(self):
-        result = requests.get(self.address+"shellr?cmd="+"adb shell input keyevent 26" ).text
+        result = requests.get(self.address+"shell?cmd="+"adb shell input keyevent 26" ).text
         print(result)
         return result
 
     def sleep(self):
-        result = requests.get(self.address+"shellr?cmd="+"adb shell input keyevent 82" ).text
+        result = requests.get(self.address+"shell?cmd="+"adb shell input keyevent 82" ).text
         print(result)
         return result
 
     def keyevent(self,keyevent):
-        result = requests.get(self.address+"shellr?cmd="+"input keyevent "+ keyevent).text
+        result = requests.get(self.address+"shell?cmd="+"input keyevent "+ keyevent).text
         print(result)
         return result
 
     def shell(self,code):
-        result = requests.get(self.address+"shellr?cmd="+code).text
+        result = requests.get(self.address+"shell?cmd="+code).text
         print(result)
         return result
 
@@ -91,14 +109,9 @@ class AutoServer:
         centery = str( (int(b2) + int(b4))/2 )
         return [centerx,centery]
 
-    def KeyValueNodeList(self,key,value,rtformat="str"):
+    def KeyValueNodeList(self,key,value):
         result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=" + key + "&value=" + value).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
+        return result
 
     def KeyValueMultiple(self,dicdata,action="tap"):
         NodeListList = []
@@ -108,191 +121,73 @@ class AutoServer:
 
 
     #return simple bound/get simple bound and tap
-    def KeyValue(self,key,value,rtformat="str",action="tap"):
+    def KeyValue(self,key,value,action="tap"):
         if action == "tap":
-            bound = json.loads(requests.get(self.address + "getboundbykeyvalue?key=" + key + "&value=" + value).text)
-            print(bound)
-            x = self.bounds2center(bound['data'])[0]
-            y = self.bounds2center(bound['data'])[1]
+            bound = self.requestdata(self.address + "getboundbykeyvalue?key=" + key + "&value=" + value)
+            #print(bound)
+            x = self.bounds2center(bound)[0]
+            y = self.bounds2center(bound)[1]
             result = self.tap(x, y)
         elif action == "null":
-            result = json.loads(requests.get(self.address + "getboundbykeyvalue?key=" + key + "&value=" + value).text)
-            print(result)
-            result = result['data']
+            result = self.requestdata(self.address + "getboundbykeyvalue?key=" + key + "&value=" + value)
+            #print(result)
+            result = result
         else:
-            result = '{"data":"action argument is incorrect","errorCode":200,"isSuccess"false}'
-        if rtformat == "str":
-            return result
-        elif rtformat == "str":
-            return json.loads(result)["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def Text(self,v,rtformat="str",action="tap"):
-        if action == "tap":
-            result = self.KeyValue("text",v,rtformat)
-            return result
-        elif action == "null":
-            result = self.KeyValue("text",v,rtformat,action="null")
-        else:
-            result = '{"data":"your argument is incorrect","errorCode":200,"isSuccess"false}'
+            result = 'action argument is incorrect'
         return result
-    def Id(self,v,action="tap"):
+    def Text(self,v,action="tap"):
         if action == "tap":
-            result = self.KeyValue("resource-id",v)
-            return result
+            result = self.KeyValue("text",v)
         elif action == "null":
-            result = self.KeyValue("resource-id",v,action="null")
+            result = self.KeyValue("text",v,action="null")
         else:
-            result = '{"data":"your argument is incorrect","errorCode":200,"isSuccess"false}'
+            result = 'your argument is incorrect'
         return result
     def Desc(self,v,action="tap"):
         if action == "tap":
             result = self.KeyValue("content-desc",v)
-            return result
         elif action == "null":
             result = self.KeyValue("content-desc",v,action="null")
         else:
-            result = '{"data":"your argument is incorrect","errorCode":200,"isSuccess"false}'
+            result = 'your argument is incorrect'
         return result
-    def Class(self,v,action="tap"):
+    def Id(self,v,action="tap"):
         if action == "tap":
-            result = self.KeyValue("class",v)
-            return result
+            result = self.KeyValue("resource-id",v)
         elif action == "null":
-            result = self.KeyValue("class",v,action="null")
+            result = self.KeyValue("resource-id",v,action="null")
         else:
-            result = '{"data":"your argument is incorrect","errorCode":200,"isSuccess"false}'
-        return result
-    def Package(self,v,action="tap"):
-        if action == "tap":
-            result = self.KeyValue("package",v)
-            return result
-        elif action == "null":
-            result = self.KeyValue("package",v,action="null")
-        else:
-            result = '{"data":"your argument is incorrect","errorCode":200,"isSuccess"false}'
-        return result
-    #not so useful
-    def Checkable(self,v,action="tap"):
-        if action == "tap":
-            result = self.KeyValue("checkable",v)
-            return result
-        elif action == "null":
-            result = self.KeyValue("checkable",v,action="null")
-        else:
-            result = '{"data":"your argument is incorrect","errorCode":200,"isSuccess"false}'
+            result = 'your argument is incorrect'
         return result
 
-
-    #return bounds/return bound list
-    def KeyValueBounds(self,key,value,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=" + key + "&value=" + value).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def TextBounds(self,v,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=text&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def IdBounds(self,v,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=resource-id&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def DescBounds(self,v,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=content-desc&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def ClassBounds(self,v,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=class&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def PackageBounds(self,v,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=package&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    #not so useful
-    def CheckableBounds(self,v,rtformat="str"):
-        result = json.loads(requests.get(self.address + "getboundsbykeyvalue?key=checkable&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-
-
-    # return nodelist
-    def TextNodeList(self, v, rtformat="str"):
-        result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=text&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def IdNodeList(self, v, rtformat="str"):
-        result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=resource-id&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def DescNodeList(self, v, rtformat="str"):
-        result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=content-desc&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def ClassNodeList(self, v, rtformat="str"):
-        result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=class&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    def PackageNodeList(self, v, rtformat="str"):
-        result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=package&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-    # not so useful
-    def CheckableNodeList(self, v, rtformat="str"):
-        result = json.loads(requests.get(self.address + "getnodelistbykeyvalue?key=checkable&value=" + v).text)
-        if rtformat == "raw":
-            return result
-        elif rtformat == "str":
-            return result["data"]
-        else:
-            return '{"data":"rtformat argument is incorrect","errorCode":200,"isSuccess"false}'
-
-
+    #return bound list
+    def KeyValueBounds(self,key,value):
+        result = self.requestdata(self.address + "getboundsbykeyvalue?key=" + key + "&value=" + value)
+        return result
+    def TextBounds(self,v):
+        result = self.requestdata(self.address + "getboundsbykeyvalue?key=text&value=" + v)
+        return result
+    def IdBounds(self,v):
+        result = self.requestdata(self.address + "getboundsbykeyvalue?key=resource-id&value=" + v)
+        return result
+    def DescBounds(self,v):
+        result = self.requestdata(self.address + "getboundsbykeyvalue?key=content-desc&value=" + v)
+        return result
+    def TextNodeList(self, v):
+        result = self.requestdata(self.address + "getnodelistbykeyvalue?key=text&value=" + v)
+        return result
+    def IdNodeList(self, v):
+        result = self.requestdata(self.address + "getnodelistbykeyvalue?key=resource-id&value=" + v)
+        return result
+    def DescNodeList(self, v):
+        result = self.requestdata(self.address + "getnodelistbykeyvalue?key=content-desc&value=" + v)
+        return result
+    def TextNode(self, v):
+        result = self.requestdata(self.address + "getnodebykeyvalue?key=text&value=" + v)
+        return result
+    def IdNode(self, v):
+        result = self.requestdata(self.address + "getnodebykeyvalue?key=resource-id&value=" + v)
+        return result
+    def DescNode(self, v):
+        result = self.requestdata(self.address + "getnodebykeyvalue?key=content-desc&value=" + v)
+        return result
